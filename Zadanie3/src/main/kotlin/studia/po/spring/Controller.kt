8465864f0f2f73ce18129f3467e55e35cece896f
Mateusz Matias <mateusz.matias@cybersentry.ai>
2025-04-08 19:01:24 +0200

    3.5 Added changes for 3.5 mark

--- Zadanie3/src/main/kotlin/studia/po/spring/Controller.kt
+++ Zadanie3/src/main/kotlin/studia/po/spring/Controller.kt
@@ -1,11 +1,15 @@
 package studia.po.spring
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
-class Controller {
+class Controller (private val authService: AuthService) {
+
     private val productList = listOf(
         Product(1, "Phone"),
         Product(2, "Charger"),
@@ -13,12 +17,22 @@ class Controller {
     )
 
     @GetMapping("/products")
-    fun getProducts(): List<Product> {
+    fun getProducts(@RequestHeader("Username") username: String, @RequestHeader("Password") password: String): List<Product> {
+        if (!authService.isAuthorized(username, password)) {
+            throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials")
+        }
         return productList
     }
 
     @GetMapping("/products/{id}")
-    fun getProduct(@PathVariable id: Int): Product {
+    fun getProduct(
+        @PathVariable id: Int,
+        @RequestHeader("Username") username: String,
+        @RequestHeader("Password") password: String
+    ): Product {
+        if (!authService.isAuthorized(username, password)) {
+            throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials")
+        }
         return productList[id - 1]
     }
 }
